bf5761b234b72c6b580ff121a5f3c94f669635a0
Aleksandr Denisenko <a.denisenko1993@gmail.com>
2023-11-26 17:42:08 +0200

    StringBuilder OutOfMemory repro

---
+++ DotNetTechConf2023/StringBuilderMemoryAlloc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < 500_000; i++)
+{
+    sb.Append(Guid.NewGuid());
+    sb.Append(Guid.NewGuid());
+    sb.Append(Guid.NewGuid());
+}
+
+sb.Clear();
