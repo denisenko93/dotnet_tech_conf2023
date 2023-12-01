238ddd9c8d4437b80811e9fbd9dbcc9a2c617703
Aleksandr Denisenko <a.denisenko1993@gmail.com>
2023-12-01 21:38:34 +0200

    mysql thread starvation fix

--- DotNetTechConf2023/MySql.Data.ThreadStarvation/Program.cs
+++ DotNetTechConf2023/MySql.Data.ThreadStarvation/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System.Diagnostics;
 using MySqlConnector;
 
+Console.WriteLine("Press any key to start application");
 Console.ReadKey();
+Console.WriteLine("Application started");
 
 var sw = Stopwatch.StartNew();
 
 await Parallel.ForAsync(1, 1000, new ParallelOptions{MaxDegreeOfParallelism = 100}, async (_, _) =>
 {
-    using MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=test;uid=root;password=root;");
-
-    connection.Open();
-    using var command = connection.CreateCommand();
-
-    command.CommandText = "DO SLEEP(1);";
-
-    await command.ExecuteNonQueryAsync();
+    await DoWorkAsync();
 });
 
 sw.Stop();
 Console.WriteLine(sw.ElapsedMilliseconds);
 
 Console.ReadKey();
+
+async Task<object?> DoWorkAsync()
+{
+    using MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=test;uid=root;password=root;ConnectionTimeout=60;");
+
+    await connection.OpenAsync();
+    using var command = connection.CreateCommand();
+
+    command.CommandText = "DO SLEEP(1); SELECT 1;";
+
+    var result = await command.ExecuteScalarAsync();
+
+    return result;
+}
