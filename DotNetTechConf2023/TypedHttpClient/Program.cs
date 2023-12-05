a9c1accc22343666b6dcbc646c361fa08dfe95f6
Aleksandr Denisenko <a.denisenko1993@gmail.com>
2023-12-05 20:42:29 +0200

    Typed http client repro

---
+++ DotNetTechConf2023/TypedHttpClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = Host.CreateApplicationBuilder(args);
+
+hostBuilder.Services.AddHttpClient<ITypedClient, TypedClient1>(x => x.BaseAddress = new Uri("http://example.com"));
+hostBuilder.Services.AddHttpClient<ITypedClient, TypedClient2>(x => x.BaseAddress = new Uri("https://github.com"));
+
+var host = hostBuilder.Build();
+
+await host.StartAsync();
+
+var clients = host.Services.GetServices<ITypedClient>();
+
+foreach (ITypedClient typedClient in clients)
+{
+    Console.WriteLine($"Client: {typedClient.GetType()}. Base url: {typedClient.Client.BaseAddress}");
+}
+
+await host.StopAsync();
+
+interface ITypedClient
+{
+    HttpClient Client { get; }
+}
+
+record TypedClient1(HttpClient Client): ITypedClient;
+record TypedClient2(HttpClient Client): ITypedClient;
